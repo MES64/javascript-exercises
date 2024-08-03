13a4a81febed2c51b53089015a5c05809d297894
Mark Skelton <markskelton10@gmail.com>
2024-08-03 12:22:20 +0100

    02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfRepeats) {
+    if (numberOfRepeats < 0) {
+        return "ERROR";
+    }
+    
+    let totalString = "";
+    for (let i=0; i<numberOfRepeats; i++) {
+        totalString += stringToRepeat;
+    }
+    return totalString;
 };
 
 // Do not edit below this line
