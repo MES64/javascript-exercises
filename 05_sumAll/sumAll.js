89ce74f54865e04a6b0f07ab424afbaa9b4c9d50
Mark Skelton <markskelton10@gmail.com>
2024-08-04 13:07:51 +0100

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || num1 <= 0 || !Number.isInteger(num2) || num2 <= 0) {
+        return "ERROR";
+    }
 
+    const increment = (num1 < num2) ? 1 : -1;
+    let sum = num2;
+    for (let i = num1; i !== num2; i += increment) sum += i;
+
+    return sum;
 };
 
 // Do not edit below this line
