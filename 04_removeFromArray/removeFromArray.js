d7e3185072d9eb67f60f0474890eddf692f1d5b9
Mark Skelton <markskelton10@gmail.com>
2024-08-04 12:37:35 +0100

    04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(ourArray, ...itemsToRemove) {
+    for (let i = ourArray.length-1; i>=0; i--) {
+        if (itemsToRemove.includes(ourArray[i])) {
+            ourArray.splice(i, 1);
+        }
+    }
+    return ourArray;
 };
 
 // Do not edit below this line
