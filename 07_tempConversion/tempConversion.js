02166599017ca49a4b820f26759a376901159d83
Mark Skelton <markskelton10@gmail.com>
2024-08-05 09:13:23 +0100

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  let temperatureInCelsius = (temperatureInFahrenheit - 32) * (5 / 9);
+  return roundToOneDecimalPlace(temperatureInCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  let temperatureInFahrenheit = temperatureInCelsius * (9 / 5) + 32;
+  return roundToOneDecimalPlace(temperatureInFahrenheit);
 };
+
+function roundToOneDecimalPlace(num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
