e169d14e9b916caa01548d75d91bee336f018ee7
Mark Skelton <markskelton10@gmail.com>
2024-08-08 12:58:46 +0100

    09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const ALPHANUMERICAL = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+const palindromes = function (string) {
+    const filtered = string
+        .toLowerCase()
+        .split("")
+        .filter((char) => ALPHANUMERICAL.includes(char));
+    const forward = filtered.join("");
+    const backward = filtered.reverse().join("");
+    return forward === backward;
 };
 
 // Do not edit below this line
