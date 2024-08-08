170a891c655309ccd996fb3dcad7dbc4a71dc11b
Mark Skelton <markskelton10@gmail.com>
2024-08-08 12:22:14 +0100

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) return 0;
+  return arr.reduce((result, num) => result * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = 1;
+	for (let i = 0; i < b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+  let result = 1;
+	for (let num = 1; num <= a; num++) {
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
