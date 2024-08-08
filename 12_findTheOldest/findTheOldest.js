9efd4ae94c0751aaa3d1523e00dc07a05b0f85e1
Mark Skelton <markskelton10@gmail.com>
2024-08-08 14:18:56 +0100

    12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people
+        .reduce((oldest, person) => (getAge(person) > getAge(oldest) ? person : oldest) , {});
 };
+
+function getAge(person) {
+    if (!("yearOfBirth" in person)) return -Infinity;
+    if ("yearOfDeath" in person) return person.yearOfDeath - person.yearOfBirth;
+    return new Date().getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
