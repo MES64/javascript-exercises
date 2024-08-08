b7815f95458df6c53f65ad006066c2d875f3598b
Mark Skelton <markskelton10@gmail.com>
2024-08-08 13:31:27 +0100

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num < 3) return 1;
+    let prevTwoNumbers = [1, 1];
+    for (let i = 2; i < num; i++) {
+        prevTwoNumbers = [prevTwoNumbers[1], prevTwoNumbers[0] + prevTwoNumbers[1]];
+    }
+    return prevTwoNumbers[1];
 };
 
 // Do not edit below this line
